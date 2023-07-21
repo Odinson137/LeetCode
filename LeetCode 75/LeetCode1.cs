df57a54d1e411ac799b72d967ecb8170013d8534
Yura <you@example.com>
2023-07-21 17:06:49 +0300

    Добавьте файлы проекта.

---
+++ LeetCode 75/LeetCode1.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text;
+
+public class Solution1
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        StringBuilder word3 = new StringBuilder();
+        int lenWord1 = word1.Length;
+        int lenWord2 = word2.Length;
+
+        int len;
+        if (lenWord1 > lenWord2) len = lenWord2;
+        else len = lenWord1;
+
+        int i;
+        for (i = 0; i < len; i++)
+        {
+            word3.Append(word1[i]);
+            word3.Append(word2[i]);
+        }
+
+        if (word2.Length > i)
+        {
+            for (; lenWord2 - 1 >= i; i++)
+                word3.Append(word2[i]);
+        }
+        else
+        {
+            for (; lenWord1 - 1 >= i; i++)
+                word3.Append(word1[i]);
+        }
+
+        return word3.ToString();
+    }
+}
---
+++ LeetCode 75/LeetCode2.cs
@@ -0,0 +1,43 @@
+﻿
+public class Solution2
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        int gcd = EuclideanGCD(str1.Length, str2.Length);
+
+        string str3 = "";
+
+        int i = 0;
+        while (i < gcd)
+        {
+            str3 += str1[i++];
+        }
+
+        int i1 = 0;
+        while (i1 < str1.Length)
+        {
+            if (str1.Substring(i1, gcd) != str3) return "";
+            i1 += gcd;
+        }
+
+        i1 = 0;
+        while (i1 < str2.Length)
+        {
+            if (str2.Substring(i1, gcd) != str3) return "";
+            i1 += gcd;
+        }
+        return str3;
+    }
+
+    public static int EuclideanGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.Abs(a);
+    }
+}
+
