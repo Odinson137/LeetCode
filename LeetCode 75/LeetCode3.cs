d2e3cd0b6707c7a7bc72ee33ceaaf3db3f1fb36d
Yura <you@example.com>
2023-07-22 22:54:34 +0300

    3, 4 (you can make it better)

---
+++ LeetCode 75/LeetCode3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode3;
+
+class LeetCode3
+{
+    private static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        //IList<bool> ret = new List<bool>(candies.Length);
+        int max = candies.Max();
+        //for (int i = 0; i < candies.Length; i++)
+        //{
+        //    if (candies[i] + extraCandies >= max)
+        //    {
+        //        ret.Add(true);
+        //    }
+        //    else ret.Add(false);
+        //}
+
+        return candies.Select(x=> (x+extraCandies) >= max).ToList();
+    }
+
+    public void Main()
+    {
+        int[] candies = new int[] { 2, 3, 5, 1, 3 };
+        int extraCandies = 3;
+        IList<bool> ret = KidsWithCandies(candies, extraCandies);
+        foreach (bool i in  ret)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
---
+++ LeetCode 75/LeetCode4.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode4;
+
+class LeetCode4
+{
+    public static bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        int len = flowerbed.Length;
+        if (len < 3)
+        {
+            if (n > 1) return false;
+            if (flowerbed.Contains(1) && n != 0) return false;
+            else return true;
+        }
+        else
+        {
+            for (int i = 0; i < len; i++)
+            {
+                if (flowerbed[i] == 1)
+                {
+                    if (i + 1 != len)
+                    {
+                        flowerbed[i + 1] = 2;
+                    }
+                    if (i - 1 != -1)
+                    {
+                        flowerbed[i - 1] = 2;
+                    }
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    sum++;
+
+                    if (i + 1 != len)
+                    {
+                        flowerbed[i + 1] = 2;
+                    }
+                    if (i - 1 != -1)
+                    {
+                        flowerbed[i - 1] = 2;
+                    }
+                }
+            }
+
+            if (sum >= n) return true;
+        }
+
+        return false;
+    }
+
+
+    //int len = flowerbed.Length;
+    //if (len < 3)
+    //{
+    //    if (n > 1) return false;
+    //    if (flowerbed.Contains(1) && n != 0) return false;
+    //    else return true;
+    //} else
+    //{
+    //    int sum = 0;
+
+    //    for (int i = 0; i < len; i++)
+    //    {
+    //        if (i + 1 != len && i - 1 != -1)
+    //        {
+    //            if (flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0 && flowerbed[i] == 0)
+    //            {
+    //                sum++;
+    //                flowerbed[i] = 1;
+    //            }
+    //        } else
+    //        {
+    //            if (flowerbed[i] == 0 && flowerbed[i+1] == 0) 
+    //            {
+    //                sum++;
+    //                flowerbed[0] = 1;
+    //            }
+    //            if (flowerbed[len - 1] == 0 && flowerbed[len - 2] == 0)
+    //            {
+    //                sum++;
+    //                flowerbed[0] = 1;
+    //            }
+    //        }
+    //    }
+
+    //    //if (flowerbed[0] == 0 && flowerbed[1] == 0) sum++;
+    //    //if (flowerbed[len-1] == 0 && flowerbed[len-2] == 0) sum++;
+    //    Console.WriteLine(string.Join("", flowerbed));
+
+    //    if (sum >= n) return true;
+    //}
+
+    //return false;
+
+    public static void Main()
+    {
+        int[] flowerbed = new int[] { 0, 1, 0, 1, 0, 1, 0, 0 };
+        int n = 2;
+        bool a = CanPlaceFlowers(flowerbed, n);
+        Console.Write(a);
+    }
+}
