7fe711e6829ec201e98284aaca01226e0fbd2580
Yura <you@example.com>
2023-07-27 12:06:20 +0300

    13 and 14 (sidling window alg. is good)

---
+++ LeetCode 75/LeetCode13.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode13;
+
+public class Solution
+{
+    public static int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        int n = 0;
+
+        int i = 0;
+        int last = nums.Length - 1;
+
+        while (i < last)
+        {
+            int sum = nums[i] + nums[last];
+            if (sum == k)
+            {
+                n++;
+                i++;
+                last--;
+
+                nums[i - 1] = 0;
+                nums[last + 1] = 0;
+            }
+
+            else if (sum < k)
+            {
+                i++;
+            } 
+            else 
+            {
+                last--;
+            }
+        }
+
+        return n;
+
+    }
+
+    //public static void Main()
+    //{
+    //    int[] nums = new int[] {
+    //        1,2,3,4
+    //    };
+
+    //    int k = 5;
+
+    //    int value = MaxOperations(nums, k);
+    //    Console.WriteLine(value);
+    //}
+}
---
+++ LeetCode 75/LeetCode14.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode14;
+
+public class Solution
+{
+    public static double FindMaxAverage(int[] nums, int k)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+
+        int max = sum;
+
+        for (int i = k; i < nums.Length; i++)
+        {
+            sum = sum - nums[i - k] + nums[i];
+            max = Math.Max(max, sum);
+        }
+
+        return (double)max / k;
+    }
+
+    //public static void Main()
+    //{
+    //    int[] nums = new int[] {
+    //        5
+    //    };
+
+    //    int k = 1;
+
+    //    double value = FindMaxAverage(nums, k);
+    //    Console.WriteLine(value);
+    //}
+}
