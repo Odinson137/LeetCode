0b079b430cab87fe9d41ddd7bf2fd5adfa594539
Yura <you@example.com>
2023-07-28 11:56:48 +0300

    solv 15 (easy) and 16 (fu***g dynamic sliding window) - look later when  you can solve it better

---
+++ LeetCode 75/LeetCode15.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode15;
+public class Solution
+{
+    public static int MaxVowels(string s, int k)
+    {
+        char[] vowels = new char[] { 'a', 'e', 'i','o','u' };
+
+        int[] mas = new int[s.Length];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (vowels.Contains(s[i]))
+            {
+                mas[i] = 1;
+            }
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            max += mas[i];
+        }
+
+        int maxVowels = max;
+
+        for (int i = k; i < mas.Length; i++)
+        {
+            max = max - mas[i - k] + mas[i];
+            if (max > maxVowels)
+            {
+                maxVowels = max;
+            }
+        }
+
+        return maxVowels;
+    }
+
+    //public static void Main()
+    //{
+    //    string s = "leetcode";
+    //    int k = 3;
+
+    //    int value = MaxVowels(s, k);
+    //    Console.WriteLine(value);
+    //}
+}
---
+++ LeetCode 75/LeetCode16.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode16;
+
+public class Solution
+{
+    public static int LongestOnes(int[] nums, int k)
+    {
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int n = 0;
+            int a = i;
+            int intK = 0;
+            while (a < nums.Length)
+            {
+                if (nums[a] == 0)
+                {
+                    intK++;
+                    if (intK > k)
+                    {
+                        break;
+                    }
+                }
+                n++;
+                a++;
+            }
+            if (n > maxLen) maxLen = n;
+
+        }
+
+        return maxLen;
+    }
+
+    //public static void Main()
+    //{
+    //    int[] nums = new int[]
+    //    {
+    //        0, 0, 0, 1
+    //    };
+    //    int k = 4;
+
+    //    int value = LongestOnes(nums, k);
+    //    Console.WriteLine(value);
+    //}
+}
