69f8998df2362d59dee276e783ea3b23b8263711
Yura <you@example.com>
2023-07-29 11:34:25 +0300

    17 (you can make it better later if you have free time) and 18 (veru well - one of the best result in site, prefix sum - my new favorite algorithm)

---
+++ LeetCode 75/LeetCode17.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode17;
+
+public class Solution
+{
+
+    //public static int LongestSubarray(int[] nums)
+    //{
+    //    int max = 0;
+    //    int n = 0;
+    //    int left = 0;
+    //    int zero = 0;
+    //    int i = 0;
+    //    while (i < nums.Length)
+    //    {
+    //        if (nums[i] == 1)
+    //        {
+    //            n++;
+    //            left++;
+    //        }
+    //        else
+    //        {
+    //            if (zero == 1)
+    //            {
+    //                max = Math.Max(max, left);
+    //                left = 0;
+    //                zero++;
+    //            } else if (zero == 2)
+    //            {
+    //                max = Math.Max(max, n);
+
+    //                n = 0;
+    //                zero = 1;
+    //            } else
+    //            {
+    //                zero++;
+    //            }
+    //        }
+    //        i++;
+    //    }
+
+    //    if (n > max) max = n;
+    //    if (!nums.Contains(0)) max = n - 1;
+
+    //    return max;
+    //}
+
+    public static int LongestSubarray(int[] nums)
+    {
+        int max = 0;
+        int n = 0;
+
+        int i = 0;
+        while (i < nums.Length)
+        {
+            if (nums[i] == 1)
+            {
+                n++;
+            }
+            else
+            {
+                int a = i + 1;
+                while (a < nums.Length)
+                {
+                    if (nums[a] == 0) break;
+                    n += 1;
+                    a++;
+                }
+                max = Math.Max(n, max);
+
+                n = 0;
+            }
+            i++;
+        }
+
+        if (n > max) max = n - 1;
+
+        return max;
+    }
+
+    //public static void Main()
+    //{
+    //    int[] nums = new int[]
+    //    {
+    //        1,1,0,0,1,1,1,0,1
+    //    };
+
+    //    int value = LongestSubarray(nums);
+    //    Console.WriteLine(value);
+    //}
+}
---
+++ LeetCode 75/LeetCode18.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode18;
+
+public class Solution
+{
+    public static int LargestAltitude(int[] gain)
+    {
+        for (int i = 1; i < gain.Length; i++)
+            gain[i] += gain[i-1];
+
+        return Math.Max(gain.Max(), 0);
+    }
+
+    //public static void Main()
+    //{
+    //    int[] gain = new int[]
+    //    {
+    //        -5,1,5,0,-7
+    //    };
+
+    //    int value = LargestAltitude(gain);
+    //    Console.WriteLine(value);
+    //}
+}
