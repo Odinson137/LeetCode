8eb2140a3e4023427ddb0852d8a369ac5678d7c0
Yura <you@example.com>
2023-07-30 11:30:10 +0300

    end prefix sum and start learning hashMaps and hashSets

---
+++ LeetCode 75/LeetCode19.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Principal;
+
+namespace LeetCode19;
+
+public class Solution
+{
+    public static int PivotIndex(int[] nums)
+    {
+        List<int> mas = new List<int>(nums);
+
+        for (int i = 1, last = nums.Length - 2; i < nums.Length; i++, last--)
+        {
+            nums[i] += nums[i - 1];
+            mas[last] += mas[last + 1];
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == mas[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //public static void Main()
+    //{
+    //    int[] gain = new int[]
+    //    {
+    //        2,1,-1
+    //    };
+
+    //    int value = PivotIndex(gain);
+    //    Console.WriteLine(value);
+    //}
+
+}
---
+++ LeetCode 75/LeetCode20.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode20
+{
+    public class Solution
+    {
+        public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            HashSet<int> answer1 = new HashSet<int>(nums1);
+            HashSet<int> answer2 = new HashSet<int>(nums2);
+
+            HashSet<int> intersection1 = new HashSet<int>(answer1);
+            intersection1.ExceptWith(answer2);
+
+            HashSet<int> intersection2 = new HashSet<int>(answer2);
+            intersection2.ExceptWith(answer1);
+
+            IList<IList<int>> mas = new List<IList<int>>() 
+            {
+                new List<int>(intersection1),
+                new List<int>(intersection2)
+            };
+
+            return mas;
+        }
+
+
+        //public static void Main()
+        //{
+        //    int[] nums1 = new int[]
+        //    {
+        //        1,2,3,3
+        //    };
+
+        //    int[] nums2 = new int[]
+        //    {
+        //        1,1,2,2
+        //    };
+
+        //    var value = FindDifference(nums1, nums2);
+        //    Console.WriteLine(value);
+        //}
+    }
+}
