35f7b4af2035202694ebc9812d86de14f335f70c
Yura <you@example.com>
2023-08-03 11:08:17 +0300

    learn Queue, which I love it now

---
+++ LeetCode 75/LeetCode27.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//RecentCounter recentCounter = new RecentCounter();
+//int[] mas = new int[]
+//{
+//    1, 100, 3001, 3002
+//};
+
+//foreach (int i in mas)
+//{
+//    int value = recentCounter.Ping(i);
+//    Console.WriteLine(value);
+//}
+
+public class RecentCounter
+{
+    Queue<int> Queue;
+
+    public RecentCounter()
+    {
+        Queue = new Queue<int>();
+    }
+
+    public int Ping(int t)
+    {
+        Queue.Enqueue(t);
+
+        while (t - Queue.Peek() > 3000)
+            Queue.Dequeue();
+
+        return Queue.Count;
+    }
+}
+
---
+++ LeetCode 75/LeetCode28.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode28;
+
+public class Solution
+{
+    public static string PredictPartyVictory(string senate)
+    {
+        Queue<char> senates = new Queue<char>(senate);
+
+        int n = 0;
+        char first = senates.Peek();
+        while (senates.Count != n)
+        {
+            char sen = senates.Dequeue();
+            if (sen == first)
+            {
+                n++;
+                senates.Enqueue(sen);
+            }
+            else
+            {
+                if (n > 0)
+                    n--;
+                else
+                {
+                    first = sen;
+                    senates.Enqueue(sen);
+                    n = 1;
+                }
+            }
+        }
+
+        if (senates.Peek() == 'R')
+            return "Radiant";
+        else
+            return "Dire";
+    }
+
+    //public static void Main()
+    //{
+    //    string senate = "RDD";
+
+    //    var value = PredictPartyVictory(senate);
+    //    Console.WriteLine(value);
+    //}
+}
+
+
