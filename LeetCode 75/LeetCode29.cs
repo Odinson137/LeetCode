8c3905161089c65a2dc70dc83d608bc21357fc9e
Yura <you@example.com>
2023-08-04 21:32:43 +0300

    learn new topic - linked list

---
+++ LeetCode 75/LeetCode29.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode29;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public static ListNode DeleteMiddle(ListNode head)
+    {
+        if (head.next == null)
+            return null;
+
+        ListNode selected = head;
+        ListNode beforeMiddle = null;
+        ListNode doubleSpeed = head;
+        while (doubleSpeed != null && doubleSpeed.next != null)
+        {
+            beforeMiddle = selected;
+            selected = selected.next;
+            doubleSpeed = doubleSpeed.next.next;
+        }
+
+        beforeMiddle.next = selected.next;
+        return head;
+    }
+
+    //public static void Main()
+    //{
+    //    //ListNode listNode = new ListNode(1, new ListNode(3, new ListNode(4, new ListNode(7, new ListNode(1, new ListNode(2, new ListNode(6)))))));
+    //    ListNode listNode = new ListNode(1);
+
+    //    var value = DeleteMiddle(listNode);
+    //    while (value.next != null)
+    //    {
+    //        Console.WriteLine(value.val);
+    //        value = value.next;
+    //    }
+    //}
+}
---
+++ LeetCode 75/LeetCode30.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode30;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public static ListNode OddEvenList(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode selected = head;
+        ListNode even = head.next;
+        ListNode temp = head.next;
+        //newHead.next = new ListNode(selected.next.next.next.val);
+        while (selected != null && selected.next != null && even != null && even.next != null)
+        {
+            selected.next = selected.next.next;
+            selected = selected.next;
+
+            even.next = even.next.next;
+            even = even.next;
+            
+        }
+        selected.next = temp;
+
+        return head;
+    }
+
+    public static void Main()
+    {
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        var value = OddEvenList(listNode);
+
+        Console.WriteLine(value.val);
+        while (value != null && value.next != null)
+        {
+            value = value.next;
+            Console.WriteLine(value.val);
+        }
+    }
+}
