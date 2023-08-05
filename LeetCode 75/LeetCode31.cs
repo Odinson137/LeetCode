ad551de43b0de6d7962cfc146dc34379ff6664ac
Yura <you@example.com>
2023-08-05 10:47:19 +0300

    end learning linked list (Stack helps me to finish it)

---
+++ LeetCode 75/LeetCode31.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode31;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public static ListNode ReverseList(ListNode head)
+    {
+        //Stack<ListNode> stack = new Stack<ListNode>();
+        //stack.Push(head);
+
+        //while (head != null && head.next != null)
+        //{
+        //    head = head.next;
+        //    stack.Push(head);
+        //}
+
+        //ListNode node = stack.Pop();
+        //ListNode a = node;
+
+        //for (; stack.Count > 0;)
+        //{
+        //    node.next = stack.Pop();
+        //    node = node.next;
+        //    node.next = null;
+        //}
+
+        //return a
+
+        ListNode cycle = head;
+        ListNode prev = null;
+
+        while (cycle != null)
+        {
+            ListNode temp = cycle.next;
+            cycle.next = prev;
+            prev = cycle;
+            cycle = temp;
+        }
+
+        return prev;
+    }
+
+    //public static void Main()
+    //{
+    //    ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+    //    var value = ReverseList(listNode);
+
+    //    Console.WriteLine(value.val);
+    //    while (value != null && value.next != null)
+    //    {
+    //        value = value.next;
+    //        Console.WriteLine(value.val);
+    //    }
+    //}
+}
---
+++ LeetCode 75/LeetCode32.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode32;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+public class Solution
+{
+    public static int PairSum(ListNode head)
+    {
+        Stack<ListNode> stack = new Stack<ListNode>();
+
+        ListNode cycle = head;
+        stack.Push(cycle);
+
+        int len = 1;
+        
+        while (cycle != null && cycle.next != null)
+        {
+            cycle = cycle.next;
+            stack.Push(cycle);
+            len++;
+        }
+
+        int max = int.MinValue;
+        int i = 0;
+        while ( len/2 > i)
+        {
+            int temp = head.val + stack.Pop().val;
+            if (temp > max) max = temp;
+            head = head.next;
+            i++;
+        }
+
+        return max;
+    }
+
+    public static void Main()
+    {
+        ListNode listNode = new ListNode(4, new ListNode(2, new ListNode(2, new ListNode(3))));
+
+        var value = PairSum(listNode);
+
+        Console.WriteLine(value);
+        
+    }
+}
