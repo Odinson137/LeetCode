c3c5732d8691bcfda292834d1efe2d5b5ba3f500
Yura <you@example.com>
2023-08-07 13:56:16 +0300

    contine learning Binary tree...

---
+++ LeetCode 75/LeetCode35.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode35;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    static int num = 0;
+    public static void Cycle(TreeNode root, int val)
+    {
+
+        if (root == null)
+        {
+            return;
+        }
+
+        if (root.val >= val)
+        {
+            num++;
+            val = root.val;
+        }
+        Cycle(root.left, val);
+        Cycle(root.right, val);
+
+    }
+
+    public static int GoodNodes(TreeNode root)
+    {
+        Cycle(root, root.val);
+        return num;
+    }
+
+    //public static void Main()
+    //{
+    //    TreeNode root1 = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+
+    //    var value = GoodNodes(root1);
+
+    //    Console.WriteLine(value);
+    //}
+
+
+}
+
+
---
+++ LeetCode 75/LeetCode36.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode36;
+
+public class Solution
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int Cycle(TreeNode root, long targetSum)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int count = 0;
+        if (targetSum == root.val)
+        {
+            count++;
+        }
+
+        count += Cycle(root.left, targetSum - root.val);
+        count += Cycle(root.right, targetSum - root.val);
+        return count;
+
+    }
+
+    public int PathSum(TreeNode root, int targetSum)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int result1 = Cycle(root, (long)targetSum);
+        int result2 = PathSum(root.left, targetSum);
+        int result3 = PathSum(root.right, targetSum);
+        return result1 + result2 + result3;
+    }
+}
