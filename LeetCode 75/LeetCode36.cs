aa3b7dd1cf09159c5c8893d0d1886d12866d4750
Yura <you@example.com>
2023-08-08 13:41:02 +0300

    contune learning binary tree, finish work with DFS and my mind slowly gets crazy....

--- LeetCode 75/LeetCode36.cs
+++ LeetCode 75/LeetCode36.cs
@@ -5,21 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LeetCode36;
-
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class Solution
 {
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
+
 
     public int Cycle(TreeNode root, long targetSum)
     {
---
+++ LeetCode 75/LeetCode37.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode37;
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution
+{
+    public TreeNode LowestCommonAncessr(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root == null || root == p || root == q)
+            return root;
+
+        TreeNode leftLCA = LowestCommonAncessr(root.left, p, q);
+        TreeNode rightLCA = LowestCommonAncessr(root.right, p, q);
+
+        if (leftLCA != null && rightLCA != null)
+            return root;
+
+        return leftLCA ?? rightLCA;
+    }
+
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        return LowestCommonAncessr(root, p, q);
+    }
+}
