10a6f75cd49effa243902ee65e1d465d626c0fec
Yura <you@example.com>
2023-08-10 11:23:45 +0300

    learn Binary search tree

---
+++ LeetCode 75/LeetCode40.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode40;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        if (root == null) return null;
+
+        if (val > root.val)
+        {
+            return SearchBST(root.right, val);
+        }
+        else if (val < root.val)
+        {
+            return SearchBST(root.left, val);
+        }
+        else
+        {
+            return root;
+        }
+        // TreeNode node = root;
+        // while (node != null) {
+        //     if (val > node.val) {
+        //         node = node.right;
+        //     } else if (val < node.val) {
+        //         node = node.left;
+        //     } else if (val == node.val) {
+        //         return node;
+        //     } else {
+        //         return null;
+        //     }
+        // }
+        // return null;
+    }
+}
---
+++ LeetCode 75/LeetCode41.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCod41;
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution
+{
+    public TreeNode DeleteNode(TreeNode root, int val)
+    {
+        if (root == null) return null;
+
+        if (val == root.val)
+        {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            TreeNode minRightSubtree = root.right;
+            while (minRightSubtree.left != null)
+            {
+                minRightSubtree = minRightSubtree.left;
+            }
+            root.val = minRightSubtree.val;
+            root.right = DeleteNode(root.right, minRightSubtree.val);
+        }
+        else if (val < root.val)
+        {
+            root.left = DeleteNode(root.left, val);
+        }
+        else
+        {
+            root.right = DeleteNode(root.right, val);
+        }
+        return root;
+    }
+}
