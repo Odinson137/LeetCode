8a25604bfe220425d814ab01cd952abe6e5649c6
Yura <you@example.com>
2023-08-11 12:18:34 +0300

    learn graphs

---
+++ LeetCode 75/LeetCode42.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode42;
+
+public class Solution
+{
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        Stack<int> stack = new Stack<int>();
+        bool[] bools = new bool[rooms.Count];
+        stack.Push(0);
+
+        while (stack.Count > 0)
+        {
+            int key = stack.Pop();
+
+            if (bools[key] == false)
+            {
+                bools[key] = true;
+                IList<int> room = rooms[key];
+
+                foreach (int k in room)
+                {
+                    stack.Push(k);
+                }
+            }
+        }
+
+        return bools.All(x => x);
+    }
+}
---
+++ LeetCode 75/LeetCode43.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LeetCode43
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
+        bool[] bools = new bool[isConnected.Length];
+        int province = 0;
+        for (int i = 0; i < isConnected.Length; i++)
+        {
+            if (bools[i] == false)
+            {
+                province++;
+                DFS(isConnected, bools, i);
+            }
+        }
+        return province;
+    }
+
+    private void DFS(int[][] isConnected, bool[] bools, int indexCity)
+    {
+        bools[indexCity] = true;
+
+        for (int i = 0; i < isConnected.Length; i++)
+        {
+            if (isConnected[indexCity][i] == 1 && bools[i] == false)
+            {
+                DFS(isConnected, bools, i);
+            }
+        }
+    }
+}
+
